1bc4184e2cd66f038a2ed6f7db3f7d929644dcde
Jason <152582224+JEX2009@users.noreply.github.com>
2025-09-03 17:21:48 -0600

    feat:text modification

--- src/components/About_me.jsx
+++ src/components/About_me.jsx
@@ -35,7 +35,7 @@ export function About_me() {
                 </div>
                 <div className='max-w-3xl'>
                     <h3 className='text-3xl font-bold mb-4 text-white'>About me</h3>
-                    <p className='text-3x0 text-white'>My journey into the world of software was born from a deep cur   iosity to transform ideas into something real. It was a fascination with the process of creating from scratch that first drove me. Since then, software development has become my passion, as it fulfills my love for constant learning and the challenge of building things that work and provide value.</p>
+                    <p className='text-3x0 text-white'>My journey into the world of software was born from a deep curiosity to transform ideas into something real. It was a fascination with the process of creating from scratch, that was the first one to drove me in. Since then, software development has become my passion, as it fulfills my love for constant learning and the challenge of building things that work and provide value.</p>
                 </div>
 
             </div>
