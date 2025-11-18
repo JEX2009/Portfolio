9be188c00ef6d4331c1e23d34da90522b152c896
Jason <152582224+JEX2009@users.noreply.github.com>
2025-11-17 21:55:06 -0600

    fix (proyects): bad update

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -1,6 +1,6 @@
 import '/src/assets/Tailwind.css'
 import { LuGithub } from "react-icons/lu";
-import TherapySystem from '../assets/TherapySystem.png' 
+import TherapySystem from '../assets/TherapySystem.png'
 import Bank from '../assets/Bank.png'
 import PosSystem from '../assets/PosSystem.png'
 
@@ -12,8 +12,7 @@ export function Proyects() {
                 <div className='h-20 w-1 bg-teal-400 rounded-full mb-2'></div>
                 <div className='h-5 w-5 bg-teal-400 rounded-full'></div>
                 <div className='h-20 w-1 bg-teal-400 rounded-full mt-2'></div>
-                
-                <h2 className="text-5xl font-extrabold mt-8 mb-4 tracking-wider text-white"> 
+                <h2 className="text-5xl font-extrabold mt-8 mb-4 tracking-wider text-white">
                     FEATURED PROJECTS
                 </h2>
                 <p className="text-center text-gray-300 text-xl max-w-3xl mb-12">
@@ -21,30 +20,30 @@ export function Proyects() {
                     Explore the code on GitHub!
                 </p>
             </div>
-            
+
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-6 max-w-7xl mx-auto'>
-                
+
                 <div className='bg-gray-800 p-6 rounded-xl shadow-2xl hover:shadow-cyan-500/50 transition-all duration-500 flex flex-col h-full'>
                     <h3 className="text-3xl font-bold text-teal-400 mb-4 text-center">
-                        Therapy Management System
+                        Website Portfolio
                     </h3>
-                    
-                    <img 
-                        src={TherapySystem} 
-                        alt="Image of the therapy system website" 
-                        className='w-full h-auto object-cover rounded-lg mb-4 border-2 border-cyan-500/50' 
-                        loading="lazy" 
+
+                    <img
+                        src={TherapySystem}
+                        alt="Imagen of the website portfolio"
+                        className='w-full h-auto object-cover rounded-lg mb-4 border-2 border-cyan-500/50'
+                        loading="lazy"
                     />
-                    
+
                     <p className="text-gray-300 text-base mb-6 flex-grow">
                         This is a full-stack project that implements a comprehensive management system for a daycare center or clinical care facility, focused on the administration of patients, medical appointments, tests, and reports. It demonstrates strong experience in building robust, secure APIs with Python (Django) and creating dynamic, responsive user interfaces with React.js. A significant challenge during the enterprise implementation was the initial learning curve involved in designing and delivering features for dynamic configuration of medical tests and the reliable, efficient generation of complex clinical reports from large datasets, skills which were successfully mastered and deployed.
                     </p>
-                    
+
                     <div className='mt-auto flex justify-center'>
-                        <a 
-                            href="https://github.com/JEX2009/Centro-Diurno-Francisca-Valerio.git" 
-                            className="inline-flex items-center justify-center gap-2 py-2 px-6 bg-cyan-600 hover:bg-cyan-500 text-white font-semibold rounded-full shadow-lg transition duration-300" 
-                            target='_blank' 
+                        <a
+                            href="https://github.com/JEX2009/Centro-Diurno-Francisca-Valerio.git"
+                            className="inline-flex items-center justify-center gap-2 py-2 px-6 bg-cyan-600 hover:bg-cyan-500 text-white font-semibold rounded-full shadow-lg transition duration-300"
+                            target='_blank'
                             rel="noopener noreferrer"
                         >
                             <LuGithub size={24} />
@@ -57,23 +56,23 @@ export function Proyects() {
                     <h3 className="text-3xl font-bold text-teal-400 mb-4 text-center">
                         Money Envelopes
                     </h3>
-                    
-                    <img 
-                        src={Bank} 
-                        alt="Image of the money envelopes system" 
-                        className='w-full h-auto object-cover rounded-lg mb-4 border-2 border-cyan-500/50' 
-                        loading="lazy" 
+
+                    <img
+                        src={Bank}
+                        alt="Imagen of the money envelopes system"
+                        className='w-full h-auto object-cover rounded-lg mb-4 border-2 border-cyan-500/50'
+                        loading="lazy"
                     />
-                    
+
                     <p className="text-gray-300 text-base mb-6 flex-grow">
                         A web application for managing personal finances using the envelope budgeting method. Developed with Django, it helps users organize their expenses and savings efficiently.
                     </p>
-                    
+
                     <div className='mt-auto flex justify-center'>
-                        <a 
-                            href="https://github.com/JEX2009/BancoConDjango.git" 
-                            className="inline-flex items-center justify-center gap-2 py-2 px-6 bg-cyan-600 hover:bg-cyan-500 text-white font-semibold rounded-full shadow-lg transition duration-300" 
-                            target='_blank' 
+                        <a
+                            href="https://github.com/JEX2009/BancoConDjango.git"
+                            className="inline-flex items-center justify-center gap-2 py-2 px-6 bg-cyan-600 hover:bg-cyan-500 text-white font-semibold rounded-full shadow-lg transition duration-300"
+                            target='_blank'
                             rel="noopener noreferrer"
                         >
                             <LuGithub size={24} />
@@ -81,28 +80,28 @@ export function Proyects() {
                         </a>
                     </div>
                 </div>
-                
+
                 <div className='bg-gray-800 p-6 rounded-xl shadow-2xl hover:shadow-cyan-500/50 transition-all duration-500 flex flex-col h-full'>
                     <h3 className="text-3xl font-bold text-teal-400 mb-4 text-center">
-                        POS System
+                        PosSystem
                     </h3>
-                    
-                    <img 
-                        src={PosSystem} 
-                        alt="Image of the money envelopes system" 
-                        className='w-full h-auto object-cover rounded-lg mb-4 border-2 border-cyan-500/50' 
-                        loading="lazy" 
+
+                    <img
+                        src={PosSystem}
+                        alt="Imagen of the money envelopes system"
+                        className='w-full h-auto object-cover rounded-lg mb-4 border-2 border-cyan-500/50'
+                        loading="lazy"
                     />
-                    
+
                     <p className="text-gray-300 text-base mb-6 flex-grow">
-                        A Point of Sale (POS) system designed for small businesses. Built with React and Django, it is still in development but aims to provide a user-friendly interface for managing sales, inventory, and customer data.
+                        A Point of Sale (POS) system designed for small businesses. Built with React and Django, it still in development but aims to provide a user-friendly interface for managing sales, inventory, and customer data.
                     </p>
-                    
+
                     <div className='mt-auto flex justify-center'>
-                        <a 
-                            href="https://github.com/JEX2009/PosSystem.git" 
-                            className="inline-flex items-center justify-center gap-2 py-2 px-6 bg-cyan-600 hover:bg-cyan-500 text-white font-semibold rounded-full shadow-lg transition duration-300" 
-                            target='_blank' 
+                        <a
+                            href="https://github.com/JEX2009/PosSystem.git"
+                            className="inline-flex items-center justify-center gap-2 py-2 px-6 bg-cyan-600 hover:bg-cyan-500 text-white font-semibold rounded-full shadow-lg transition duration-300"
+                            target='_blank'
                             rel="noopener noreferrer"
                         >
                             <LuGithub size={24} />
@@ -111,4 +110,6 @@ export function Proyects() {
                     </div>
                 </div>
             </div>
-        </
+        </>
+    )
+}
